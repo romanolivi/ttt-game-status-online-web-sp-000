19ae0969976f291028475aff5639ecd6f6ab55f7
Romano Livi <romano_livi@outlook.com>
2020-12-09 21:24:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1index)
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return single_win_combo
     end
   end
